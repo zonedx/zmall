10de208eb5f739f2bbe88408c54a12eb2aefea8c
xin.duan <xin.duan@cygia.com>
2019-10-12 16:35:55 +0800

    commit springSession

---
+++ src/main/java/com/zmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,93 @@
+package com.zmall.controller.portal;
+
+import com.zmall.common.Const;
+import com.zmall.common.ResponseCode;
+import com.zmall.common.ServerResponse;
+import com.zmall.pojo.User;
+import com.zmall.service.IUserService;
+import com.zmall.util.CookieUtil;
+import com.zmall.util.JsonUtil;
+import com.zmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * SpringSession实现session共享  对业务没有侵入
+ * 大量减少重复代码
+ *
+ * @ClassName: UserController
+ * @Date 2019-09-15 01:53
+ * @Author duanxin
+ **/
+
+@Controller
+@RequestMapping("/user/springSession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    /**
+     * 用户登录
+     *
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+
+//            CookieUtil.writeLoginToken(httpServletResponse,session.getId());
+//            RedisShardedPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+        }
+        return response;
+    }
+
+    /**
+     * 登出
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session,HttpServletRequest request,HttpServletResponse response) {
+        session.removeAttribute(Const.CURRENT_USER);
+//        String loginToken = CookieUtil.readLoginToken(request);
+//        CookieUtil.delLoginToken(request,response);
+//        RedisShardedPoolUtil.del(loginToken);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest httpServletRequest) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if (StringUtils.isEmpty(loginToken)){
+//            return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息");
+//        }
+//
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr,User.class);
+
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息");
+    }
+
+}

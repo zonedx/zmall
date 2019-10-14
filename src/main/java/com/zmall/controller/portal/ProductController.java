27ad477f4d3df8ae23c6bc47520d7bdacfaff533
xin.duan <xin.duan@cygia.com>
2019-10-14 10:27:45 +0800

    commit productController with restful

--- src/main/java/com/zmall/controller/portal/ProductController.java
+++ src/main/java/com/zmall/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.zmall.common.ServerResponse;
 import com.zmall.service.IProductService;
 import com.zmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName: ProductController
@@ -29,6 +28,13 @@ public class ProductController {
         return iProductService.getProductDetail(productId);
     }
 
+    @RequestMapping(value = "/{productId}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTFul(@PathVariable Integer productId) {
+        return iProductService.getProductDetail(productId);
+    }
+
+//    http://www.dlzonemall.com/product/list/手机/100002/1/10/price_asc/
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<PageInfo> list(@RequestParam(value = "keyword", required = false) String keyword,
@@ -38,4 +44,64 @@ public class ProductController {
                                          @RequestParam(value = "orderBy",defaultValue = "")String orderBy) {
         return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
     }
+
+    @RequestMapping(value = "/list/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTFul(@PathVariable(value = "keyword") String keyword,
+                                         @PathVariable(value = "categoryId") Integer categoryId,
+                                         @PathVariable(value = "pageNum") Integer pageNum,
+                                         @PathVariable(value = "pageSize") Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy) {
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+//    http://www.dlzonemall.com/product/keyword/手机/1/10/price_asc/
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTFul(@PathVariable(value = "keyword") String keyword,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy) {
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+
+//    http://www.dlzonemall.com/product/categoryId/100002/1/10/price_asc/
+    @RequestMapping(value = "/categoryId/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTFul(@PathVariable(value = "categoryId") Integer categoryId,
+                                                @PathVariable(value = "pageNum") Integer pageNum,
+                                                @PathVariable(value = "pageSize") Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy) {
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory("",categoryId,pageNum,pageSize,orderBy);
+    }
+
+
 }
